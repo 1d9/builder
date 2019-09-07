c472906b9833b85d1ad683baa982577429c3db4e
Luke Kaalim <lukekaalim@gmail.com>
2019-09-07 21:15:19 +1000

    Update builder to use latest version of plugin

--- install_deps.sh
+++ install_deps.sh
@@ -23,13 +23,13 @@ unzip terraform.zip -d ~/bin
 rm -f terraform.zip
 
 # Download terraform-aws-elastic-beanstalk-stateless-versions
-terraform_stateless_url="https://github.com/lukekaalim/terraform-aws-elastic-beanstalk-stateless-versions/releases/download/v1.0.1/terraform-provider-aws-uncontrolled"
+terraform_stateless_url="https://github.com/lukekaalim/terraform-aws-elastic-beanstalk-stateless-versions/releases/download/v1.0.2/terraform-provider-aws-uncontrolled"
 wget -O terraform-provider-aws-uncontrolled -q $terraform_stateless_url
 # Put the plugin inside the terraform global plugins folder
 mkdir -p ~/.terraform.d/plugins
 cp terraform-provider-aws-uncontrolled ~/.terraform.d/plugins
 # Mark as executable
-chmod 777 ~/.terraform.d/plugins/terraform-provider-aws-uncontrolled
+chmod +x ~/.terraform.d/plugins/terraform-provider-aws-uncontrolled
 # Clean up
 rm -f terraform-provider-aws-uncontrolled
 
